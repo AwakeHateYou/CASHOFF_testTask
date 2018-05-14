1b625d2cd828993a9c2e6db41e2931161e9bf801
Евгений Терентьев <eterentev@diasoft.ru>
2018-05-14 21:57:25 +0300

    Сохранение в localStorage и удаление

--- src/bankDirectory.js
+++ src/bankDirectory.js
@@ -1,5 +1,6 @@
 var bankList = [],
-    lastCard;
+    lastCard,
+    storage = localStorage;
 
 var saveUtils = {
     nvl: function (id) {
@@ -11,13 +12,49 @@ var saveUtils = {
             BIK: this.nvl("edBankBIK"),
             CorrAccount: this.nvl("edCorrespondentAccount"),
             Address: this.nvl("edAddress"),
-            LinkID: bankList.length
+            LinkID: bankList.length,
+            id: "crdBank" + bankList.length
         });
+        localStorage.setItem("bankList", JSON.stringify(bankList));
     },
     saveBankInfo: function () {
-        console.log("Save in progress");
-        this.createCrd();
-        this.placeCard();
+        if (form.checkValidity()) {
+            console.log("Save in progress");
+            this.createCrd();
+            this.placeCard();
+        }
+    },
+    processBankList: function(topItemFlag){
+        var startFrom = topItemFlag ? bankList.length - 1 : 0;
+        for (var i = startFrom; i < bankList.length; i++){
+            if (i !== 0) {
+                var lastElem = document.getElementById(bankList[i - 1].id);
+                var newCard = lastElem.cloneNode(true);
+            } else {
+                var newCard = document.getElementById("crdBank0");
+            }
+            var item = bankList[i];
+            newCard.id = item.id;
+            newCard.querySelector("h5").innerText = item.Name;
+            newCard.querySelectorAll("p").forEach(function (value) {
+                switch (value.id) {
+                    case "crdBIK":
+                        value.innerText = item.BIK;
+                        break;
+                    case "crdCorrAccount":
+                        value.innerText = item.CorrAccount;
+                        break;
+                    case "crdAddress":
+                        value.innerText = item.Address;
+                        break;
+                    default:
+                        break;
+                }
+            });
+            if (i !== 0) {
+                lastElem.parentNode.insertBefore(newCard, lastElem.nextSibling);
+            }
+        }
     },
     placeCard: function () {
         var index = bankList.length - 1,
@@ -30,10 +67,11 @@ var saveUtils = {
             document.getElementById("bankList").style.visibility = "visible";
         } else {
             if (bankList.length > 1) {
-                var newCard = crdBank.cloneNode(true);
-                newCard.id = newCard.id + (item.LinkID);
+                var lastElem = document.getElementById(bankList[index - 1].id);
+                var newCard = lastElem.cloneNode(true);
+                newCard.id = item.id;
                 newCard.querySelector("h5").innerText = item.Name;
-                newCard.querySelectorAll("li").forEach(function (value) {
+                newCard.querySelectorAll("p").forEach(function (value) {
                     switch (value.id) {
                         case "crdBIK":
                             value.innerText = item.BIK;
@@ -48,18 +86,46 @@ var saveUtils = {
                             break;
                     }
                 });
-                crdBank.parentNode.insertBefore(newCard, crdBank.nextSibling);
+                lastElem.parentNode.insertBefore(newCard, lastElem.nextSibling);
             }
         }
+        this.clearInput();
+        this.changeListVisibility();
     },
     deleteBank: function (context) {
-        var parentID = context.offsetParent.id;
-        document.getElementById(parentID).remove();
+        var parentID = context.parentElement.offsetParent.id;
+        if (bankList.length !== 1 && parentID) {
+            document.getElementById(parentID).remove();
+        }
+
+        var newBankList = [];
+        for (var i = 0; i < bankList.length; i++) {
+            if (parentID !== bankList[i].id) {
+                newBankList.push(bankList[i]);
+            }
+        }
+        bankList = newBankList;
+        this.changeListVisibility();
+        localStorage.setItem("bankList", JSON.stringify(bankList));
+    },
+    changeListVisibility: function () {
+        if (bankList.length !== 0) {
+            document.getElementById("bankList").style.visibility = "visible";
+        } else {
+            document.getElementById("bankList").style.visibility = "hidden";
+        }
+    },
+    clearInput: function () {
+        document.getElementById("edBankName").value = "";
+        document.getElementById("edBankBIK").value = "";
+        document.getElementById("edCorrespondentAccount").value = "";
+        document.getElementById("edAddress").value = "";
     }
 };
 
 var onShow = function () {
-    if (bankList.length !== 0) {
-        document.getElementById("bankList").style.visibility = "visible";
-    }
+    var storeString = localStorage.getItem("bankList");
+    bankList = storeString ? JSON.parse(storeString) : [];
+    saveUtils.processBankList();
+    saveUtils.changeListVisibility();
 };
